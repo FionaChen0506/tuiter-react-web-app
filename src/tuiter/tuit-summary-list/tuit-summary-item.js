2cd8d56dbc588b27b797e6f561eddf090dd2e6b3
Shaoyujie Chen <chenshaoyujie@ShaoyujieChens-MacBook-Air.local>
2023-08-15 15:55:05 -0700

    5th commit for a6

--- src/tuiter/tuit-summary-list/tuit-summary-item.js
+++ src/tuiter/tuit-summary-list/tuit-summary-item.js
@@ -17,8 +17,10 @@ const TuitSummaryItem = (
    <div className="row">
      <div className="col-10">
        <div>{tuit.handle} · {tuit.time}</div>
-       <div className="fw-bolder">{tuit.topic}</div>
-       <div>{tuit.title}</div>
+       {/* <div className="fw-bolder">{tuit.topic}</div>
+       <div>{tuit.title}</div> */}
+       <div className="fw-bolder">{tuit.userName}</div>
+       <div>{tuit.tuit}</div>
      </div>
      <div className="col-2">
        <img width={70} className="float-end rounded-3" src={`/images/${tuit.image}`}/>
